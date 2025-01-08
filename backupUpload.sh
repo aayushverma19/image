0e77d779f01596a62ffe16bf79450cfb8ac8a9d2
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-01-09 00:41:04 +0530

    backupUpload.sh

--- backupUpload.sh
+++ backupUpload.sh
@@ -1,22 +1,45 @@
 #!/bin/bash
-#mysql-backup-tool-bucket
+# mysql-backup-tool-bucket
 
-read -p "Enter the S3 bucket name: " BUCKET_NAME
-read -p "Enter the path to save the backup: " BACKUP_PATH
-read -p "Enter the MySQL database username: " DB_USER
-read -sp "Enter the MySQL database password: " DB_PASS
+# Set default values
+DEFAULT_BUCKET_NAME="mysql-backup-tool-bucket"
+DEFAULT_BACKUP_PATH="/tmp/backup"
+DEFAULT_DB_USER="aayush"
+DEFAULT_DB_PASS="1234"  
+DEFAULT_DB_NAME="ninja"
+
+# Prompt for user input with default values
+read -p "Enter the S3 bucket name [$DEFAULT_BUCKET_NAME]: " BUCKET_NAME
+BUCKET_NAME=${BUCKET_NAME:-$DEFAULT_BUCKET_NAME}
+
+read -p "Enter the path to save the backup [$DEFAULT_BACKUP_PATH]: " BACKUP_PATH
+BACKUP_PATH=${BACKUP_PATH:-$DEFAULT_BACKUP_PATH}
+
+read -p "Enter the MySQL database username [$DEFAULT_DB_USER]: " DB_USER
+DB_USER=${DB_USER:-$DEFAULT_DB_USER}
+
+# Prompt for password and use the default if nothing is entered
+read -sp "Enter the MySQL database password (press enter to use default password): " DB_PASS
 echo
-read -p "Enter the MySQL database name: " DB_NAME
+
+# If no password is entered, use the default password
+if [ -z "$DB_PASS" ]; then
+  DB_PASS=$DEFAULT_DB_PASS
+  echo "Using default password."
+fi
+
+read -p "Enter the MySQL database name [$DEFAULT_DB_NAME]: " DB_NAME
+DB_NAME=${DB_NAME:-$DEFAULT_DB_NAME}
 
 # MySQL database and gzip the backup
-mysqldump -u $DB_USER -p$DB_PASS $DB_NAME > $BACKUP_PATH.sql
-gzip $BACKUP_PATH.sql
+mysqldump -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" > "$BACKUP_PATH.sql"
+gzip "$BACKUP_PATH.sql"
 
 # Upload the backup to S3
-aws s3 cp $BACKUP_PATH.sql.gz s3://$BUCKET_NAME/backups/
+aws s3 cp "$BACKUP_PATH.sql.gz" s3://"$BUCKET_NAME"/backups/
 
-# after cp remove file
-rm -rf $BACKUP_PATH.sql.gz
+# After cp remove file
+rm -rf "$BACKUP_PATH.sql.gz"
 
 # Verify the backup upload
 if [ $? -eq 0 ]; then
